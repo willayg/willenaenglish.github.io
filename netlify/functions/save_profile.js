85e4d283d1cf500b90b77704f14b7224d1c6a6d8
willayg <william.gaule@gmail.com>
2025-06-15 12:42:23 +0900

    Profile: save user id with profile, update RLS policy, and improve save logic

--- netlify/functions/save_profile.js
+++ netlify/functions/save_profile.js
@@ -6,7 +6,7 @@ exports.handler = async (event) => {
   }
 
   const SUPABASE_URL = process.env.SUPABASE_URL;
-  const SUPABASE_KEY = process.env.SUPABASE_KEY;
+  const SUPABASE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
   const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
   const data = JSON.parse(event.body);
