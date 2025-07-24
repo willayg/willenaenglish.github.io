802ed811924d3dcab47556c03944df574957581f
willayg <william.gaule@gmail.com>
2025-07-25 01:25:46 +0900

    Force push: worksheet builder React app and updates

---
+++ Teachers/tools/worksheet-builder/src/BurgerMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function BurgerMenu() {
+  return (
+    <div id="burger-menu-mount">
+      <span style={{width: '24px', height: '3px', background: 'white', borderRadius: '2px', display: 'block'}}></span>
+      <span style={{width: '24px', height: '3px', background: 'white', borderRadius: '2px', display: 'block'}}></span>
+      <span style={{width: '24px', height: '3px', background: 'white', borderRadius: '2px', display: 'block'}}></span>
+    </div>
+  );
+}
