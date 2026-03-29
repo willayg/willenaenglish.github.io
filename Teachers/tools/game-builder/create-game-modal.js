5b3207cc935c1b88bafacb30a76c1c77b91bdf6f
willayg <william.gaule@gmail.com>
2026-03-29 20:06:00 +0900

    Fix builder ex_kor reload mapping

--- Teachers/tools/game-builder/create-game-modal.js
+++ Teachers/tools/game-builder/create-game-modal.js
@@ -15,7 +15,7 @@
 // -------------------------------------------------------------
 
 import { preprocessTTS } from '../../../Games/english_arcade/tts.js';
-import { getCurrentGameId, setCurrentGameId } from './state/game-state.js?v=20260328e';
+import { getCurrentGameId, setCurrentGameId } from './state/game-state.js?v=20260329f';
 import { ensureAudioForWordsAndSentences } from './services/audio-service.js';
 import { openPixabayImagePicker } from './images.js?v=20260329a';
 
