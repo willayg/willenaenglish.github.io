ed2b2ac0568717d6cf195a1932cd32dbe85336cd
willayg <william.gaule@gmail.com>
2026-08-09 19:20:32 +0900

    Bump Study question bank level mapping asset

--- students/study/smart-study-screen.js
+++ students/study/smart-study-screen.js
@@ -7,7 +7,7 @@ addCss('./study-coach.css?v=20260809-phase1b');
  try{
   await load('/shared/learning-engine/adaptive-study.js?v=20260809-phase1');
   await load('./study-question-policy.js?v=20260809-phase1');
-  await load('./study-question-bank.js?v=20260809-grammarfix');
+  await load('./study-question-bank.js?v=20260809-levelmap');
   await load('./study-coach.js?v=20260809-phase1b');
   await load('./daily-workout.js?v=20260809-publiclevel');
   await load('./study-practice-override.js?v=20260809-phase1');
