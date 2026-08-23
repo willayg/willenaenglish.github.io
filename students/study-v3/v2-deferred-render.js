015cebabbaa6c318bd488a50e90dd3d9f4754506
willayg <william.gaule@gmail.com>
2026-08-24 01:25:21 +0900

    Remove Coach travelling spark loader

--- students/study-v3/v2-deferred-render.js
+++ students/study-v3/v2-deferred-render.js
@@ -26,25 +26,10 @@ proto.__v2DeferredSetActivity=true;
 (function(){
 'use strict';
 if(location.hostname!=='staging.willenaenglish.com')return;
-if(!document.querySelector('script[data-v3-question-flagger]')){
-  var s=document.createElement('script');
-  s.src='./v3-question-flagger.js?v=20260823-flagger1';
-  s.async=false;
-  s.dataset.v3QuestionFlagger='1';
-  document.head.appendChild(s);
-}
-if(!document.querySelector('link[data-v3-coach-travelling-spark]')){
-  var l=document.createElement('link');
-  l.rel='stylesheet';
-  l.href='./v3-coach-travelling-spark.css?v=20260824-1';
-  l.dataset.v3CoachTravellingSpark='1';
-  document.head.appendChild(l);
-}
-if(!document.querySelector('script[data-v3-coach-travelling-spark]')){
-  var c=document.createElement('script');
-  c.src='./v3-coach-travelling-spark.js?v=20260824-1';
-  c.async=false;
-  c.dataset.v3CoachTravellingSpark='1';
-  document.head.appendChild(c);
-}
+if(document.querySelector('script[data-v3-question-flagger]'))return;
+var s=document.createElement('script');
+s.src='./v3-question-flagger.js?v=20260823-flagger1';
+s.async=false;
+s.dataset.v3QuestionFlagger='1';
+document.head.appendChild(s);
 })();
