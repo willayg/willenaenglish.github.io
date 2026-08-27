4f332d66f28107cb2313ee2c92ca482252a6eb99
willayg <william.gaule@gmail.com>
2026-08-28 01:17:11 +0900

    Bust cache for fixed Naesin stats

--- Teachers/dashboard-v2/naesin.js
+++ Teachers/dashboard-v2/naesin.js
@@ -1,8 +1,8 @@
 (function(){
 'use strict';
 function load(src){return new Promise((resolve,reject)=>{const s=document.createElement('script');s.src=src;s.onload=resolve;s.onerror=reject;document.head.appendChild(s)})}
-load('./naesin-core-fresh.js?v=20260828-ui2')
-  .then(()=>load('./naesin-signedoff-ui.js?v=20260828-ui2'))
+load('./naesin-core-fresh.js?v=20260828-ui3')
+  .then(()=>load('./naesin-signedoff-ui.js?v=20260828-ui3'))
   .then(()=>load('./naesin-history.js?v=20260828-nav1'))
   .catch(e=>console.error('[naesin-loader]',e));
 })();
