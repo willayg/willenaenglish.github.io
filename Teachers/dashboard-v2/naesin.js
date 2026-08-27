1c68e2f98c60bc450b70610e7be811267363d9ac
willayg <william.gaule@gmail.com>
2026-08-27 23:40:32 +0900

    Disable crashing Naesin student analytics module

--- Teachers/dashboard-v2/naesin.js
+++ Teachers/dashboard-v2/naesin.js
@@ -6,7 +6,6 @@ function load(src){return new Promise((resolve,reject)=>{const s=document.create
     await load('./naesin-core.js?v=20260827-core1');
     await load('./naesin-dynamic-catalog.js?v=20260827-catalog1');
     await load('./naesin-dashboard-v2.js?v=20260827-ui2');
-    await load('./naesin-student-analytics.js?v=20260827-naanalytics1');
   }catch(e){console.error('[naesin] failed to load dashboard modules',e)}
 })();
 })();
