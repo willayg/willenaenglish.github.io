1643b64ec414a7f9181e9add3c50bcce18d541e5
willayg <william.gaule@gmail.com>
2026-08-29 00:00:21 +0900

    Bust cache for test prep wizard fix

--- Teachers/dashboard-v2/naesin.js
+++ Teachers/dashboard-v2/naesin.js
@@ -8,6 +8,6 @@ load('./naesin-core-fresh.js?v=20260828-ui3')
   .then(()=>load('./naesin-diagnostic-fix.js?v=20260828-wrong3'))
   .then(()=>load('./naesin-delete.js?v=20260828-delete1'))
   .then(()=>load('./naesin-mobile-polish.js?v=20260828-style2'))
-  .then(()=>load('./naesin-scope-selector-fix.js?v=20260828-scope1'))
+  .then(()=>load('./naesin-scope-selector-fix.js?v=20260828-scope2'))
   .catch(e=>console.error('[naesin-loader]',e));
 })();
