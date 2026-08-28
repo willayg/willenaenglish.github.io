a3d6bebfad8ef8e841f2de8698ee7c0478ebca3c
willayg <william.gaule@gmail.com>
2026-08-29 00:07:11 +0900

    Decouple Naesin loader from dashboard shell

--- Teachers/dashboard-v2/naesin.js
+++ Teachers/dashboard-v2/naesin.js
@@ -1,7 +1,6 @@
 (function(){
 'use strict';
 function load(src){return new Promise((resolve,reject)=>{const s=document.createElement('script');s.src=src;s.onload=resolve;s.onerror=reject;document.head.appendChild(s)})}
-load('./dashboard-shell-polish.js?v=20260828-command3');
 load('./naesin-core-fresh.js?v=20260828-ui3')
   .then(()=>load('./naesin-signedoff-ui.js?v=20260828-ui3'))
   .then(()=>load('./naesin-history.js?v=20260828-nav1'))
