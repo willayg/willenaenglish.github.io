7c3948ec87f6a8a4979a44821f81be4562af1724
willayg <william.gaule@gmail.com>
2026-08-28 21:41:24 +0900

    Cache bust Naesin submenu border polish

--- Teachers/dashboard-v2/naesin.js
+++ Teachers/dashboard-v2/naesin.js
@@ -6,6 +6,6 @@ load('./naesin-core-fresh.js?v=20260828-ui3')
   .then(()=>load('./naesin-history.js?v=20260828-nav1'))
   .then(()=>load('./naesin-diagnostic-fix.js?v=20260828-wrong3'))
   .then(()=>load('./naesin-delete.js?v=20260828-delete1'))
-  .then(()=>load('./naesin-mobile-polish.js?v=20260828-style1'))
+  .then(()=>load('./naesin-mobile-polish.js?v=20260828-style2'))
   .catch(e=>console.error('[naesin-loader]',e));
 })();
