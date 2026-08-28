23acf2d6afce1e1dddc14a827b5309dc1c46cdc4
willayg <william.gaule@gmail.com>
2026-08-28 23:09:04 +0900

    Cache bust collapsed sidebar polish

--- Teachers/dashboard-v2/naesin.js
+++ Teachers/dashboard-v2/naesin.js
@@ -7,7 +7,7 @@ load('./naesin-core-fresh.js?v=20260828-ui3')
   .then(()=>load('./naesin-diagnostic-fix.js?v=20260828-wrong3'))
   .then(()=>load('./naesin-delete.js?v=20260828-delete1'))
   .then(()=>load('./naesin-mobile-polish.js?v=20260828-style2'))
-  .then(()=>load('./dashboard-shell-polish.js?v=20260828-shell4'))
+  .then(()=>load('./dashboard-shell-polish.js?v=20260828-shell5'))
   .then(()=>load('./naesin-scope-selector-fix.js?v=20260828-scope1'))
   .catch(e=>console.error('[naesin-loader]',e));
 })();
