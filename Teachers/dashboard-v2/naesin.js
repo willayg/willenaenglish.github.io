fc9f7cb75e6be0e0f686b46a0fa27ae77dc2a6ad
willayg <william.gaule@gmail.com>
2026-08-28 20:54:31 +0900

    Load Naesin test delete controls

--- Teachers/dashboard-v2/naesin.js
+++ Teachers/dashboard-v2/naesin.js
@@ -5,5 +5,6 @@ load('./naesin-core-fresh.js?v=20260828-ui3')
   .then(()=>load('./naesin-signedoff-ui.js?v=20260828-ui3'))
   .then(()=>load('./naesin-history.js?v=20260828-nav1'))
   .then(()=>load('./naesin-diagnostic-fix.js?v=20260828-wrong3'))
+  .then(()=>load('./naesin-delete.js?v=20260828-delete1'))
   .catch(e=>console.error('[naesin-loader]',e));
 })();
