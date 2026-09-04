36548cc661d0a6da38ebf1c2318484e81dca0ff8
willayg <william.gaule@gmail.com>
2026-09-05 01:24:22 +0900

    REV22 finish vocab test single-owner cleanup

--- students/test-prep-app/vocab-reference-bridge.js
+++
@@ -1,5 +0,0 @@
-(function(){
-'use strict';
-// REV21: bridge retired as a runtime owner. Stored-question loading/mixing now lives in vocab-test-upgrade.js.
-window.WillenaVocabReferenceBridge={retired:true,owner:'vocab-test-upgrade.js'};
-})();
