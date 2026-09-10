d3861fce5ccdeea3ee6d398911ffe22e265d7213
willayg <william.gaule@gmail.com>
2026-09-11 00:10:08 +0900

    chore: remove unused session helper

--- students/test-prep-v2/session-events.js
+++
@@ -1,3 +0,0 @@
-const EVENT='testprep:v2-activity-snapshot';
-export function requestActivitySnapshot(){try{window.dispatchEvent(new CustomEvent(EVENT))}catch(_){}}
-export function onActivitySnapshotRequest(handler){if(typeof handler!=='function')return()=>{};window.addEventListener(EVENT,handler);return()=>window.removeEventListener(EVENT,handler)}
