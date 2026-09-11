13e55eae3194720f17e2ca03265a075edd85d7d3
willayg <william.gaule@gmail.com>
2026-09-12 01:36:59 +0900

    Point level test v2 to 3-question speaking module

--- students/level-test-v2/app-phase5.js
+++ students/level-test-v2/app-phase5.js
@@ -1,6 +1,6 @@
 import {createVisitorIntake} from './visitor-intake.js?v=20260911-phase5';
 import {createNavigator} from './navigation.js?v=20260911-phase5';
-import {createSpeakingAssessment} from './speaking-assessment.js?v=20260912-phase5q5';
+import {createSpeakingAssessment} from './speaking-assessment-3q.js?v=20260912-phase5q6';
 import {
   SESSION_PHASE,
   SESSION_STATUS,
