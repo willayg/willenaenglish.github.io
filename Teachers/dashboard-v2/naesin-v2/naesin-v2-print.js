2b8ae1c32b00275f6d92edf59f8e1ccaf473b2f6
willayg <william.gaule@gmail.com>
2026-09-12 16:40:37 +0900

    Add Naesin V2 print bridge skeleton

---
+++ Teachers/dashboard-v2/naesin-v2/naesin-v2-print.js
@@ -0,0 +1,7 @@
+(function(){
+'use strict';
+window.NaesinV2Print={
+  openEditor(){console.info('[Naesin V2] print editor bridge not wired yet');},
+  makePdf(){console.info('[Naesin V2] direct PDF bridge not wired yet');}
+};
+})();
