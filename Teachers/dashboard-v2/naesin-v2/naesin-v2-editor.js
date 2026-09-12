a6b96eb77cca69e86cdb6c78fd8acdd2ca3a9003
willayg <william.gaule@gmail.com>
2026-09-12 16:40:18 +0900

    Add Naesin V2 editor skeleton

---
+++ Teachers/dashboard-v2/naesin-v2/naesin-v2-editor.js
@@ -0,0 +1,7 @@
+(function(){
+'use strict';
+window.NaesinV2Editor={
+  openCreate(){console.info('[Naesin V2] create editor not wired yet');},
+  openEdit(groupId){console.info('[Naesin V2] edit editor not wired yet',groupId);}
+};
+})();
