1da0665b76afeb89dd1dfa43ed81ebb26fa8caf2
willayg <william.gaule@gmail.com>
2026-09-15 01:11:00 +0900

    Route Grammar Foundations progress to Netlify backend

--- students/api-gateway.js
+++ students/api-gateway.js
@@ -22,7 +22,7 @@
   const NETLIFY_ONLY_FUNCTIONS = new Set([
     'verify_student','set_student_password','debug_student_data','openai_proxy','google_vision_proxy',
     'supabase_proxy','supabase_proxy_fixed','teacher_admin','test_admin','eleven_labs_proxy','translate','define_word',
-    'student_study_current','progress_summary'
+    'student_study_current','progress_summary','grammar_foundations_progress'
   ]);
   const FORCE_GATEWAY_FUNCTIONS = new Set([
     'upsert_sentences_batch',
