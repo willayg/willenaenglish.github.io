c1dc5d190df21bd7a4527590f6153b77746e261f
willayg <william.gaule@gmail.com>
2026-09-15 00:14:19 +0900

    Load full grammar guide content on students app

--- students/test-prep-v2/header-v2.js
+++ students/test-prep-v2/header-v2.js
@@ -1,3 +1,4 @@
+import './grammar-guide-extra.js?v=1.0.0';
 import {startStudentHeaderData,subscribeStudentHeaderData} from '../shared/student-header-data.js?v=1.0.0';
 import {logoutStudent,openStudentProfile} from '../shared/student-header-actions.js?v=1.0.1';
 
