f7f0b9c0327997db7ebc1e836190d0e7545da25c
willayg <william.gaule@gmail.com>
2026-09-18 23:02:24 +0900

    Bump Test Prep sequencer module version

--- students/test-prep-v2/practice-loader.js
+++ students/test-prep-v2/practice-loader.js
@@ -2,7 +2,7 @@
 // Owns practice-type content selection and ordinary question sequencing.
 import {loadStoredSkill,loadStoredWritten} from './content-source.js?v=2.24.0';
 import {loadVocabularyTest} from './vocab-test-source.js?v=2.14.0';
-import {buildQuestionQueue} from '../shared/question-sequencer.js?v=1.0.0';
+import {buildQuestionQueue} from '../shared/question-sequencer.js?v=1.0.1';
 import {loadActivitySnapshot,saveActivityQueue,clearActivitySnapshotFor} from './activity-session-store.js?v=4.0.0';
 
 const OLIVIA_ID='70a422d5-9587-481e-9f05-4a3c3aeb96b1';
