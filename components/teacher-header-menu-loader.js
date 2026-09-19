efe90b4b60046eee3f5cd584a134bba9efe401d0
willayg <william.gaule@gmail.com>
2026-09-19 18:00:36 +0900

    Bump teacher header menu cache key

--- components/teacher-header-menu-loader.js
+++ components/teacher-header-menu-loader.js
@@ -1,5 +1,5 @@
-const TEMPLATE_URL='/components/burger-menu.html?v=20260919-header-menu-2';
-const MODULE_URL='/components/burger-menu.js?v=20260919-header-menu-2';
+const TEMPLATE_URL='/components/burger-menu.html?v=20260919-header-menu-3';
+const MODULE_URL='/components/burger-menu.js?v=20260919-header-menu-3';
 
 export async function mountTeacherHeaderMenu(){
   if(!document.getElementById('burger-menu-template')){
