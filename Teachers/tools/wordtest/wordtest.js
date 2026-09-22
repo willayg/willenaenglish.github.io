2ea7771afddf4873bac13e988cbf54df9b4a30cf
willayg <william.gaule@gmail.com>
2026-09-22 23:56:47 +0900

    Rollback Word Builder module cache change

--- Teachers/tools/wordtest/wordtest.js
+++ Teachers/tools/wordtest/wordtest.js
@@ -1,11 +1,11 @@
 // Word Worksheet Generator - Slim entry point
 // Keep this file tiny: wire up globals and bootstrap the app.
 
-import { initWordtest } from './init.js?v=20260922-wb2011';
-import { updatePreview } from './preview.js?v=20260922-wb2011';
-import { printFile, generatePDF } from './print.js?v=20260922-wb2011';
-import { getCurrentWorksheetData, loadWorksheet } from './worksheet_integration.js?v=20260922-wb2011';
-import { cycleImage, setSelectedImage } from './images.js?v=20260922-wb2011';
+import { initWordtest } from './init.js';
+import { updatePreview } from './preview.js';
+import { printFile, generatePDF } from './print.js';
+import { getCurrentWorksheetData, loadWorksheet } from './worksheet_integration.js';
+import { cycleImage, setSelectedImage } from './images.js';
 
 // Expose a minimal surface for inline handlers and external pages
 window.cycleImage = (word, index) => cycleImage(word, index, updatePreview);
