2dade873afa600a0aa0981a92e72b16d1b308f23
willayg <william.gaule@gmail.com>
2026-09-22 23:49:50 +0900

    Cache-bust Word Builder module graph 20260922-wb2011

--- Teachers/tools/wordtest/controls.js
+++ Teachers/tools/wordtest/controls.js
@@ -1,5 +1,5 @@
-import { state } from './state.js';
-import { updatePreview, updatePreviewStyles } from './preview.js';
+import { state } from './state.js?v=20260922-wb2011';
+import { updatePreview, updatePreviewStyles } from './preview.js?v=20260922-wb2011';
 import {
     updateFont as uiUpdateFont,
     updateFontSize as uiUpdateFontSize,
@@ -8,7 +8,7 @@ import {
     updateImageGap as uiUpdateImageGap,
     updateImageSize as uiUpdateImageSize,
     updateLayout as uiUpdateLayout
-} from './ui.js';
+} from './ui.js?v=20260922-wb2011';
 
 const currentSettings = state.currentSettings;
 
