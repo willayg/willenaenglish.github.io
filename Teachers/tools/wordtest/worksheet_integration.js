046a367947d1a5d2f790eef99fdc61ab3380c0db
willayg <william.gaule@gmail.com>
2026-09-22 23:52:45 +0900

    Cache-bust worksheet integration imports

--- Teachers/tools/wordtest/worksheet_integration.js
+++ Teachers/tools/wordtest/worksheet_integration.js
@@ -1,12 +1,12 @@
-import { state } from './state.js';
-import { updatePreview, updatePreviewPreservingImages } from './preview.js';
-import { resetImageState } from './images.js';
+import { state } from './state.js?v=20260922-wb2011';
+import { updatePreview, updatePreviewPreservingImages } from './preview.js?v=20260922-wb2011';
+import { resetImageState } from './images.js?v=20260922-wb2011';
 import {
     highlightDuplicates as worksheetHighlightDuplicates,
     getCurrentWorksheetData as worksheetGetCurrentWorksheetData,
     loadWorksheet as worksheetLoadWorksheet,
     updateCurrentWordsFromTextarea as worksheetUpdateCurrentWordsFromTextarea
-} from './worksheet.js';
+} from './worksheet.js?v=20260922-wb2011';
 
 const currentWords = state.currentWords;
 const currentSettings = state.currentSettings;
