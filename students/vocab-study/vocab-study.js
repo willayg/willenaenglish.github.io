3f1b960dce9690c3f9a47636398f48c0c0a5329c
willayg <william.gaule@gmail.com>
2026-09-25 10:47:51 +0900

    Bump vocab shared renderer cache for hint fix

--- students/vocab-study/vocab-study.js
+++ students/vocab-study/vocab-study.js
@@ -1,4 +1,4 @@
-import {QuestionRenderer} from '/shared/questions/question-renderer.js?v=20260924-spelling3';
+import {QuestionRenderer} from '/shared/questions/question-renderer.js?v=20260925-spelling4';
 
 const SESSION_SIZE=12;
 const CONTENT_URL='https://gxwfsqxyuufqtitspfqg.supabase.co';
