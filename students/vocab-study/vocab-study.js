0a165e44a66a0a2163f3c7076c1c4cb8b031a970
willayg <william.gaule@gmail.com>
2026-09-26 11:44:20 +0900

    Set Vocabulary Study v0.053

--- students/vocab-study/vocab-study.js
+++ students/vocab-study/vocab-study.js
@@ -1753,7 +1753,7 @@ async function boot(){
       renderSkillProgress();
     });
     reportStartupPerf();
-    window.WillenaVocabStudy={version:'0.052',getState:()=>state,start:startSession,openSpellingMenu,openSpellingPreview,openSpellingTest,openSpeakingSession,close:closeSession};
+    window.WillenaVocabStudy={version:'0.053',getState:()=>state,start:startSession,openSpellingMenu,openSpellingPreview,openSpellingTest,openSpeakingSession,close:closeSession};
   }catch(error){
     console.error('[Vocab Study] boot',error);
     setStatus(error?.message||'불러오지 못했습니다. 새로고침해 주세요.');
